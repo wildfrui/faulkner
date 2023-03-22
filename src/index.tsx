2ca01852c3a4ab6a132bbd7022b92e1e321bf0ed
Artyom Rebrik <wildfrui@gmail.com>
2023-03-22 23:55:25 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import App from "app";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(<App></App>);
